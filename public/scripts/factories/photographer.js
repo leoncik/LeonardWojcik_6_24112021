36283e44d1e161de9378fcd3a971595d11b3baa5
Léonard Wojcik <leonardw@tutanota.com>
2021-11-24 15:49:43 +0100

    fix(index): typo in JS script. - Fixed typo in JS script name called in index.html - Fixed directory location in photographer.js

--- public/scripts/factories/photographer.js
+++ public/scripts/factories/photographer.js
@@ -1,7 +1,7 @@
 function photographerFactory(data) {
     const { name, portrait } = data;
 
-    const picture = `assets/photographers/${portrait}`;
+    const picture = `public/assets/photographers/${portrait}`;
 
     function getUserCardDOM() {
         const article = document.createElement( 'article' );
