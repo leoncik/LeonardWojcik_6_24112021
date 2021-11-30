67d2ba5b64bb0aafe13e51aee9e98d0619807c6e
Léonard Wojcik <leonardw@tutanota.com>
2021-11-30 20:05:49 +0100

    feat: fetch data and display photographers name

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -5,6 +5,9 @@ All notable changes to this project will be documented in this file.
 The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
 and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
 
+## [0.0.4] - 2021-11-30
+- Fetch photographer data and display photographer names on index page.
+
 ## [0.0.3] - 2021-11-25
 ### Added
 - Fetch photographer data.
--- public/scripts/pages/index.js
+++ public/scripts/pages/index.js
@@ -1,63 +1,62 @@
-    async function getPhotographers() {
-        // Penser à remplacer par les données récupérées dans le json
-        const photographers = [
-            {
-                "name": "Ma data test",
-                "id": 1,
-                "city": "Paris",
-                "country": "France",
-                "tagline": "Ceci est ma data test",
-                "price": 400,
-                "portrait": "account.png"
-            },
-            {
-                "name": "Autre data test",
-                "id": 2,
-                "city": "Londres",
-                "country": "UK",
-                "tagline": "Ceci est ma data test 2",
-                "price": 500,
-                "portrait": "account.png"
-            },
-        ]
-        // Fetch data
-        // **** First version ****
-        fetch('./src/data/photographers.json')
-            .then(res => {
-                if(res.ok){
-                    return res.json().then((data) => {
-                        console.log(data);
-                        return data;
-                    })
-                } else {
-                    console.log("ERROR, can't fetch the data file");
-                }
-            })
-        /* **** Second version **** */
-        /*
-        let response = await fetch('../../../src/data/photographers.json');
-        let fullData = await response.json(); NOTE : pas besoin de mettre de await ici.
-        console.log(fullData);*/
-        // et bien retourner le tableau photographers seulement une fois
-        return ({
-            photographers: [...photographers, ...photographers, ...photographers]})
+let photographerData;
+
+async function getPhotographers() {
+    let photographers; 
+    // Penser à remplacer par les données récupérées dans le json
+    /*
+    const photographers = [
+        {
+            "name": "Ma data test",
+            "id": 1,
+            "city": "Paris",
+            "country": "France",
+            "tagline": "Ceci est ma data test",
+            "price": 400,
+            "portrait": "account.png"
+        },
+        {
+            "name": "Autre data test",
+            "id": 2,
+            "city": "Londres",
+            "country": "UK",
+            "tagline": "Ceci est ma data test 2",
+            "price": 500,
+            "portrait": "account.png"
+        },
+    ]*/
+    // Fetch data
+    const response = await fetch('./src/data/photographers.json');
+    if(response.ok) {
+        photographerData = await response.json();
+        // Save photographer list (type array)
+        photographers = photographerData.photographers;
+        console.log(photographers);
+        // Photographer list (type object)
+        console.log({photographers});
+        return  {photographers};
+    } else {
+        console.log("ERROR, can't fetch the data file");
     }
+    // et bien retourner le tableau photographers seulement une fois
+    //return ({
+    //    photographers: [...photographers, ...photographers, ...photographers]})
+}
 
-    async function displayData(photographers) {
-        const photographersSection = document.querySelector(".photographer_section");
 
-        photographers.forEach((photographer) => {
-            const photographerModel = photographerFactory(photographer);
-            const userCardDOM = photographerModel.getUserCardDOM();
-            photographersSection.appendChild(userCardDOM);
-        });
-    };
+async function displayData(photographers) {
+    const photographersSection = document.querySelector(".photographer_section");
 
-    async function init() {
-        // Récupère les datas des photographes
-        const { photographers } = await getPhotographers();
-        displayData(photographers);
-    };
-    
-    init();
-    
+    photographers.forEach((photographer) => {
+        const photographerModel = photographerFactory(photographer);
+        const userCardDOM = photographerModel.getUserCardDOM();
+        photographersSection.appendChild(userCardDOM);
+    });
+};
+
+async function init() {
+    // Récupère les datas des photographes
+    const { photographers } = await getPhotographers();
+    displayData(photographers);
+};
+
+init();
