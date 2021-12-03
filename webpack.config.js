0c1490bd62cd90cc7ed4c10e6f2c5126663c7003
Léonard Wojcik <leonardw@tutanota.com>
2021-12-03 17:23:15 +0100

    test: config webpack server

--- webpack.config.js
+++ webpack.config.js
@@ -1,27 +1,27 @@
 const path = require('path');
 
 module.exports = {
-  mode: "production",
-  entry: {
-    polyfill: "babel-polyfill",
-    app: "./public/scripts/pages/index.js"
-  },
-  output: {
-    filename: "[name].bundle.js",
-    path: path.resolve(__dirname, "dist")
-  },
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: {
-          loader: "babel-loader",
-          options: {
-            presets: ["@babel/preset-env"]
-          }
-        }
-      }
-    ]
-  }
+    mode: 'production',
+    entry: {
+        polyfill: 'babel-polyfill',
+        app: './src/scripts/pages/index.js',
+    },
+    output: {
+        filename: '[name].bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env'],
+                    },
+                },
+            },
+        ],
+    },
 };
