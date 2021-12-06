82ca6c898894149f0cbba4457bc3db74ea7d0ec8
Léonard Wojcik <leonardw@tutanota.com>
2021-12-06 11:38:05 +0100

    fix: return homepage path - Fix path to return to the home page. - Add comments in photographer.js

--- src/scripts/factories/photographer.js
+++ src/scripts/factories/photographer.js
@@ -5,27 +5,40 @@ export default function photographerFactory(data) {
 
     function getUserCardDOM() {
         const article = document.createElement('article');
+
+        // Redirection link
         const photographerLink = document.createElement('a');
         photographerLink.setAttribute(
             'href',
             `./pages/photographer-pages/photographer.html?id=${id}`
         );
+        article.appendChild(photographerLink);
+
+        // Profile picture
         const img = document.createElement('img');
         img.setAttribute('src', picture);
+        photographerLink.appendChild(img);
+
+        // Photographer name
         const h2 = document.createElement('h2');
         h2.textContent = name;
+        photographerLink.appendChild(h2);
+
+        // Location
         const location = document.createElement('p');
         location.textContent = city;
+        article.appendChild(location);
+
+        // Motto
         const motto = document.createElement('p');
         motto.textContent = tagline;
+        article.appendChild(motto);
+
+        // Pricing
         const pricing = document.createElement('p');
         pricing.textContent = price;
-        article.appendChild(photographerLink);
-        photographerLink.appendChild(img);
-        photographerLink.appendChild(h2);
-        article.appendChild(location);
-        article.appendChild(motto);
         article.appendChild(pricing);
+
         return article;
     }
     // Why return getUserCardDOM ?
