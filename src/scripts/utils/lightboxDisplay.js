6cc475ce1fbf48588c55158fee7ba257fec1b118
Léonard Wojcik <leonardw@tutanota.com>
2021-12-26 17:23:55 +0100

    refactor(lightboxDisplay): media display - Trying to create a tag to display media instead of only changing the "src".

--- src/scripts/utils/lightboxDisplay.js
+++ src/scripts/utils/lightboxDisplay.js
@@ -20,8 +20,27 @@ export function lightboxDisplay() {
     for (const iterator of galleryItems) {
         iterator.addEventListener('click', () => {
             displayLightbox();
-            // TODO : if mediatype = video, set img tag to video (or use two tags in HTML ?)
             const mediaSrc = iterator.querySelector('.media').src;
+            // TEST if mediatype = image, create img tag (if not already here)
+            if (mediaSrc.split('.').pop() === 'jpg') {
+                document.querySelector(
+                    '.current-media-factory-method'
+                ).innerHTML = ' ';
+                const imageTag = document.createElement('img');
+                document
+                    .querySelector('.current-media-factory-method')
+                    .appendChild(imageTag);
+                imageTag.src = mediaSrc;
+            } else if (mediaSrc.split('.').pop() === 'mp4') {
+                document.querySelector(
+                    '.current-media-factory-method'
+                ).innerHTML = ' ';
+                const videoTag = document.createElement('video');
+                document
+                    .querySelector('.current-media-factory-method')
+                    .appendChild(videoTag);
+                videoTag.src = mediaSrc;
+            }
             currentMedia.src = mediaSrc;
         });
     }
