ebbfab11ee9190cf5314a38946685bac16b16741
Léonard Wojcik <leonardw@tutanota.com>
2022-01-17 20:30:55 +0100

    fix: set JS index path to work with owner's repo

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -12,9 +12,11 @@ const main = async () => {
     switch (window.location.pathname) {
         case '/':
         case '/index.html':
+        case '/LeonardWojcik_6_24112021/':
             await initHome();
             break;
         case '/pages/photographer-pages/photographer.html':
+        case '/LeonardWojcik_6_24112021/pages/photographer-pages/photographer.html':
             await initPhotographer();
             break;
     }
